05038c1a7d97f5957575c22beacc55a50bd2edb0
kolen <incredible.angst@gmail.com>
2020-05-29 17:38:40 +0300

    Apply formating

--- lib/osm/xml.ex
+++ lib/osm/xml.ex
@@ -2,18 +2,22 @@ defmodule OSM.XML do
   @doc """
   Parses OSM XML file into maps of nodes, ways and relations.
   """
-  @spec parse_stream(any) :: OSM.dataset
+  @spec parse_stream(any) :: OSM.dataset()
   def parse_stream(stream) do
-    {:ok, {data, nil, nil}} = Saxy.parse_stream(stream, OSM.XML.Handler, {{%{}, %{}, %{}}, nil, nil})
+    {:ok, {data, nil, nil}} =
+      Saxy.parse_stream(stream, OSM.XML.Handler, {{%{}, %{}, %{}}, nil, nil})
+
     data
   end
 
   @doc """
   Parses OSM XML file into maps of nodes, ways and relations.
   """
-  @spec parse_string(String.t) :: OSM.dataset
+  @spec parse_string(String.t()) :: OSM.dataset()
   def parse_string(string) do
-    {:ok, {data, nil, nil}} = Saxy.parse_string(string, OSM.XML.Handler, {{%{}, %{}, %{}}, nil, nil})
+    {:ok, {data, nil, nil}} =
+      Saxy.parse_string(string, OSM.XML.Handler, {{%{}, %{}, %{}}, nil, nil})
+
     data
   end
 end
