c6c1e23ced344c6c0e4de58b7b85e993cce91181
kolen <incredible.angst@gmail.com>
2020-06-19 22:42:43 +0300

    Apply formatting

--- lib/pt_checker/fetch.ex
+++ lib/pt_checker/fetch.ex
@@ -1,14 +1,16 @@
 defmodule PtChecker.Fetch do
   def fetch(relation_id) do
-    resp_body = PtChecker.OverpassAPI.query("""
-    [timeout:5];
-    (
-      relation(#{relation_id});
-    );
-    (._; >>;);
-    (._; >;);
-    out body;
-    """)
+    resp_body =
+      PtChecker.OverpassAPI.query("""
+      [timeout:5];
+      (
+        relation(#{relation_id});
+      );
+      (._; >>;);
+      (._; >;);
+      out body;
+      """)
+
     OSM.XML.parse_string(resp_body)
   end
 end
