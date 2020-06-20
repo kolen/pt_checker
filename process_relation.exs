1c0c437e624510b31d231ff6ddd766d1dfd04d21
kolen <incredible.angst@gmail.com>
2020-06-20 22:51:26 +0300

    Apply formatting to process_relation

--- process_relation.exs
+++ process_relation.exs
@@ -1,5 +1,5 @@
-[relation_id_s] = System.argv
-relation_id = String.to_integer relation_id_s
-dataset = PtChecker.Fetch.fetch relation_id
+[relation_id_s] = System.argv()
+relation_id = String.to_integer(relation_id_s)
+dataset = PtChecker.Fetch.fetch(relation_id)
 result = PtChecker.Checker.check_group_root(dataset, relation_id)
-IO.inspect result
+IO.inspect(result)
