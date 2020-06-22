4c8e3166aaee1f07d21c04cfeccd372e269487e3
kolen <incredible.angst@gmail.com>
2020-06-23 02:14:28 +0300

    Remove info_version_old message

--- lib/pt_checker/checks/informational.ex
+++ lib/pt_checker/checks/informational.ex
@@ -19,8 +19,6 @@ defmodule PtChecker.Checks.Informational do
        not Map.has_key?(rel.tags, "name") && not Map.has_key?(rel.tags, "ref")},
       {{:info_no_version_2, :notice, [{:relation, relation_id}]},
        not Map.has_key?(rel.tags, "public_transport:version")},
-      {{:info_version_old, :error, [{:relation, relation_id}]},
-       rel.tags["public_transport:version"] == "1"},
       {{:info_no_operator, :notice, [{:relation, relation_id}]},
        not Map.has_key?(rel.tags, "operator")},
       {{:info_no_network, :notice, [{:relation, relation_id}]},
